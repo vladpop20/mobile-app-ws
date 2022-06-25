e258f81ce6156f950626d70975eaa01f3cd9ecd0
Vlad-PC <vladpop20@gmail.com>
2022-06-25 17:43:04 +0300

    creating the AuthorizationFilter, and use this class after a user has logged successfully - part2

--- src/main/java/com/coodru/mobile/app/ws/security/AuthorizationFilter.java
+++ src/main/java/com/coodru/mobile/app/ws/security/AuthorizationFilter.java
@@ -1,6 +1,9 @@
 package com.coodru.mobile.app.ws.security;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -8,10 +11,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /*
-Once a user logged in using "localhost/login" or "/user/login" (as the endpoint I created),
-	the user received an Authorization token and a UserID.
+Once a user logged in using "localhost/login" or "/user/login" (endpoint that I created),
+	the user will receive an Authorization token and a UserID.
 E.g:
 	Authorization -> Bearer eyJhbGciOiJIUzUxMiJ9
 	.eyJzdWIiOiJnZW8udGVzdEBnbWFpbC5jb20iLCJleHAiOjE2NTY5NDYxMDh9
@@ -19,7 +23,7 @@ E.g:
 
 	UserID -> lmvrm75to8vyayNn7tqtjcvsq
 
-Those 2 will be used by this class (Authorization-filter), in order to execute any operations on endpoint.
+Those 2 parameters, will be used by this class (AuthorizationFilter), in order to execute any operations on some future endpoint.
  */
 public class AuthorizationFilter extends BasicAuthenticationFilter {
 
@@ -28,12 +32,41 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
 		super(authenticationManager);
 	}
 
+
+	@Override
 	protected void doFilterInternal(HttpServletRequest request,
 									HttpServletResponse response,
 									FilterChain filterChain) throws IOException, ServletException {
 
 		String header = request.getHeader(SecurityConstants.HEADER_STRING);
 
+		if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
 
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		filterChain.doFilter(request, response);
+	}
+
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(SecurityConstants.HEADER_STRING);
+
+		if (token != null) {
+			token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+			String user = Jwts.parser().setSigningKey(SecurityConstants.TOKEN_SECRET)
+					.parseClaimsJws(token)
+					.getBody()
+					.getSubject();
+
+			if (user != null) {
+				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+		}
+
+		return null;
 	}
 }
