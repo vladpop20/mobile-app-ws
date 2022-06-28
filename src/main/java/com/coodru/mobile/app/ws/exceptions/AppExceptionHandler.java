b0e3eb489c053dbeefadd96566f8289b2e2c2a75
Vlad-PC <vladpop20@gmail.com>
2022-06-28 14:05:40 +0300

    return a custom Error object representation

---
+++ src/main/java/com/coodru/mobile/app/ws/exceptions/AppExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.coodru.mobile.app.ws.exceptions;
+
+import com.coodru.mobile.app.ws.ui.controller.model.response.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+	@ExceptionHandler(value = {UserServiceException.class})
+	public ResponseEntity<?> handleUserServiceException(UserServiceException ex, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
