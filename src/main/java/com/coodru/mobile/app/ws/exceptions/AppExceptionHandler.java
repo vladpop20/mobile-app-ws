9776f666be0a7fe942b8d602f6bff58e68627121
Vlad-PC <vladpop20@gmail.com>
2022-06-28 16:59:43 +0300

    handle all others exceptions

--- src/main/java/com/coodru/mobile/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/coodru/mobile/app/ws/exceptions/AppExceptionHandler.java
@@ -19,4 +19,11 @@ public class AppExceptionHandler {
 
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<?> handleOtherException(Exception ex, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
