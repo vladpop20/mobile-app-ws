3e699120253745de36297b0246d7bf89013ab884
Vlad-PC <vladpop20@gmail.com>
2022-07-02 11:09:04 +0300

    modification for deployment, using Tomcat, and WAR for packaging.

--- src/main/java/com/coodru/mobile/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/coodru/mobile/app/ws/MobileAppWsApplication.java
@@ -2,8 +2,16 @@ package com.coodru.mobile.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication public class MobileAppWsApplication {
+@SpringBootApplication public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
